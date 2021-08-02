716324186fca261acd7695f67908561ccaa3e076
lidorg-dev <39281113+lidorg-dev@users.noreply.github.com>
2021-08-02 21:12:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, DevOps!');
+    res.end('Hello, Moshe!');
     
 }).listen(process.env.PORT || 8080);
